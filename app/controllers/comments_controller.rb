884d65be48a9c56657fa1e3c2640169110b66f90
Brayder Berroteran <brayderb@gmail.com>
2019-03-03 23:00:13 -0300

    first commit

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,11 @@
+class CommentsController < ApplicationController
+  
+  def create 
+    @comment = Comment.new(
+      content: params[:comment][:content],
+      post: Post.find(params[:post_id])
+    )
+    @comment.save
+    respond_to :js
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+
+  def index
+    if params[:buscador].present? && params[:buscador].length >2
+      @result = Post.where('title like ?', "%#{params[:buscador]}%")
+    else
+      @post = Post.new
+      @posts = Post.order(:id)
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id]) 
+    respond_to :js
+  end
+
+  def create
+    @post = Post.new(title: params[:post][:title])
+    @post.user = current_user
+    @post.save 
+    respond_to :js
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    respond_to :js
+  end
+
+  def edit
+    @post = Post.find(params[:id]) 
+    respond_to :js
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.title = params[:post][:title]
+    @post.save
+    respond_to :js
+  end
+end
